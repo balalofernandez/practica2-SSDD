c5afd1d71f76607b69b08018e1d4595dc5b2f21b
Diego <Diego@DESKTOP-3ISS0PP>
2021-05-14 13:15:51 +0200

    brfkbgljhber

--- Practica2/src/main/java/practica2/entities/Especie.java
+++ Practica2/src/main/java/practica2/entities/Especie.java
@@ -17,6 +17,7 @@ public class Especie {
 	
 	private String nombreVulgar;
 	private String nombreCientifico;
+	private String URL;
 	@ManyToMany
 	private List<Plaga> listaPlagas;
 	@ManyToMany
@@ -34,11 +35,31 @@ public class Especie {
 	public void setNombreCientifico(String nombreCientifico) {
 		this.nombreCientifico = nombreCientifico;
 	}
+	public String getURL() {
+		return URL;
+	}
+	public void setURL(String uRL) {
+		URL = uRL;
+	}
 	public List<Plaga> getListaPlagas() {
 		return listaPlagas;
 	}
+	public List<CategoriaDeCultivo> getListaCategorias() {
+		return listaCategorias;
+	}
+	
 	public void setListaPlagas(List<Plaga> listaPlagas) {
 		this.listaPlagas = listaPlagas;
 	}
+	public void setListaCategorias(List<CategoriaDeCultivo> listaCategorias) {
+		this.listaCategorias = listaCategorias;
+	}
 	
+	public void modifyEspecie(Especie especie) {
+		this.nombreVulgar = especie.getNombreVulgar();
+		this.listaPlagas = especie.getListaPlagas();
+		this.nombreCientifico = especie.getNombreCientifico();
+		this.URL = especie.getURL();
+		this.listaCategorias = especie.getListaCategorias();		
+	}
 }
---
+++ Practica2/src/main/java/practica2/restControllers/EspecieRestController.java
@@ -0,0 +1,47 @@
+package practica2.restControllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import practica2.entities.*;
+import practica2.repository.EspecieRepository;
+
+@RestController
+public class EspecieRestController {
+
+	@Autowired
+	private EspecieRepository especiesRep;
+
+	@RequestMapping(value = "/especies", method = RequestMethod.GET)
+	public List<Especie> getEspecie() {
+		return especiesRep.findAll();
+	}
+
+	@RequestMapping(value = "/especies/{id}", method = RequestMethod.GET)
+	public Especie getEspecie(@PathVariable("id") long id) {
+		return especiesRep.getOne(id);
+	}
+	
+
+	@RequestMapping(value = "/especies", method = RequestMethod.POST)
+	public ResponseEntity<Boolean> addCultivo(@RequestBody Especies especie) {
+		this.especiesRep.save(especie);
+		return new ResponseEntity<Boolean>(true,HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(value = "/especies/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Boolean> modifyPelicula(@PathVariable("id") long id,@RequestBody Especie especie) {
+		Especie esp = especiesRep.getOne(id);
+		esp.updateEspecie(especie);
+		especiesRep.save(esp);
+		return new ResponseEntity<Boolean>(true,HttpStatus.OK);
+	}
+}
