7192c4532e72cbb8d14b62073932b34e6c1c3c3c
balalofernandez <balalo.fernandez@gmail.com>
2021-05-20 20:13:54 +0200

    javascript

--- Practica2/src/main/resources/static/JavaScript/getPlagas.js
+++ Practica2/src/main/resources/static/JavaScript/getPlagas.js
@@ -12,7 +12,7 @@ function generarPlagas(lista,idpl){
 				nestedList.append($('<li class="list-group-item">').append(
 					$(`<div class="row ${plaga} "/>`).append(
 						$('<span class="plagas col-10">')
-							.html(respuesta[plaga].nombreVulgar,respuesta[plaga].nombreCientifico, respuesta[plaga].URL)
+							.html(`${respuesta[plaga].nombreVulgar}, ${respuesta[plaga].nombreCientifico}, <a href="${respuesta[plaga].uRL}">${respuesta[plaga].uRL}<a/>`)
 					).append(
 						$(`<span class="material-icons col-2 btn btn-light plagas">`).attr('id', `${respuesta[plaga].idPlaga}`).html("visibility")
 				)))
